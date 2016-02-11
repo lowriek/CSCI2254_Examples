14a5c059707bec5c489089975dff1bc975ae06d1
K Lowrie <lowriekate@gmail.com>
2016-02-10 21:46:59 -0500

    Change attire to css.  Also got rid of div in the selector.
    there is not div child of td

---
+++ W4_Listhandler/js/listhandlerjQ.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#moveright').click(function () {
+  
+  	var contents = $('ul#leftlist li' ).last().remove().html();
+  	if (undefined != contents) {
+  		$('ul#rightlist').append("<li>" + contents + "</li>");
+	}  	
+  });
+  $('#moveleft').click(function () {
+  
+  	var contents = $( 'ul#rightlist li' ).last().remove().html();
+  	if (undefined != contents) {
+  		$('ul#leftlist').append("<li>" + contents + "</li>");
+	}  	
+  	
+  });
+});
