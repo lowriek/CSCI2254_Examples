4cb0994dab9c5d728c23240f021712f84002767c
K Lowrie <lowriekate@gmail.com>
2016-02-14 10:33:30 -0500

    Added workout summary calculations
    Used each() function to get the workout times from all the entered
    workouts.

--- W5_jQuery3/js/workout.js
+++ W5_jQuery3/js/workout.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+
   $('#logworkout').click(function () {
   
+  	var workoutstats = new Array();
+  	workoutstats = workouttotals();
+  	
   	var workoutdate = $('#newworkoutdate').val();
   	var newworkoutdescription = $('#newworkoutdescription').val();
   	
@@ -14,5 +18,20 @@ $(document).ready(function() {
   							     '<td class="description">' + newworkoutdescription + '</td>' + 
   							     '<td ="type">' + newworkouttype + '</td>'+
   							     '<td class="duration">' + newworkoutduration + '</td></tr>');
+
+    $('#workouttimes').text(++workoutstats[1]);
+    $('#workoutminutes').text(workoutstats[0] + parseInt(newworkoutduration));
+  });
+  
+  function workouttotals(){
+  	var totalminutes = 0;
+ 	var count = 0;
+ 	
+  	$('.duration').each(function() {
+  		var cminutes = $(this).text();
+  		totalminutes += parseInt(cminutes);
+  		count ++;
   	});
-  } );
+  	return ([totalminutes, count]);
+  }
+});
