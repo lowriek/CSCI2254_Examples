9fa9854e24361f8b102398c1784f2e6da4900a2e
K Lowrie <lowriekate@gmail.com>
2016-02-22 20:45:47 -0500

    Added first form

---
+++ W6_FirstPHP/myfirstphpform.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>My First PHP Form</title>
+</head>
+<body>
+<?php echo "<pre>". print_r($_GET) . "</pre>"; ?>
+<h1>My First PHP Form</h1>
+<fieldset><legend>The First Form</legend>
+    <form method="get">
+    First: <input type="text" name="count1"/><br>
+    Second: <input type="text" name="count2"/>
+    <input type="submit"  name="mysubmit" value="Go"/>
+</form>
+</fieldset>
+<br><br>
+<?php if (isset($_GET['mysubmit'])) handleform();?>
+</body>
+</html>
+<?php
+function handleform(){
+    $a = $_GET['count1'];
+    $b = $_GET['count2'];
+    $sum = $a + $b;
+    echo "  <fieldset><legend>The results</legend>
+                The sum is $sum
+            </fieldset>";
+}
+?>
