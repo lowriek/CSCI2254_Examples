5cfd26f0cbdc3116617c79777c23bff363c57d65
K Lowrie <lowriekate@gmail.com>
2016-02-25 11:26:11 -0500

    Style updates

--- W6_Password/password_validate.php
+++ W6_Password/password_validate.php
@@ -9,7 +9,7 @@
 <body>
 <pre><?php //print_r($_GET); ?></pre>
 	Password demo
-<? 
+<?php 
 		if ( isset( $_POST['op'] ) )
 			handle_form( $_POST['op'] );
 		
@@ -19,20 +19,20 @@
 ?>
 </body>
 </html>
-<? function handle_form($op) 
+<?php function handle_form( $op ) 
 {
-		$entered_name = $_POST['name'];	
+		$entered_name   = $_POST['name'];	
 		$entered_passwd = $_POST['pass'];
 		
-		switch ($op) {
+		switch ( $op ) {
 		case "insert":
-			insert_user($entered_name, $entered_passwd);
+			insert_user( $entered_name, $entered_passwd );
 			break;
 		case "validate":
-			validate_user($entered_name, $entered_passwd);	
+			validate_user( $entered_name, $entered_passwd) ;	
 			break;
 		default:
-			die("Invalid operation");
+			die( "Invalid operation" );
 	}	
 }
 
@@ -42,11 +42,12 @@ function display_validate_form() {
 		Name:  
 		<input type="text" name="name" size="30" maxlength="60" value=""/>
 		<br><br>
-		Password:	<input type="password" name="pass" size="30" maxlength="80">		<br />
+		Password:	<input type="password" name="pass" size="30" maxlength="80">
+		<br />
 		<input type="hidden" name="op" value="validate" />
 		<input type="submit" name="validate" value="Validate!">
 	</form></fieldset>	<br>
-<?
+<?php
 }
 function display_insert_form() {
 ?>
@@ -54,18 +55,19 @@ function display_insert_form() {
 		Name:  
 		<input type="text" name="name" size="30" maxlength="60" value=""/>
 		<br><br>
-		Password:	<input type="password" name="pass" size="30" maxlength="80">		<br />
+		Password:	<input type="password" name="pass" size="30" maxlength="80">
+		<br />
 		<input type="hidden" name="op" value="insert" />
 		<input type="submit" name="insert" value="Insert!">
 	</form></fieldset>	<br>
-<?
+<?php
 }
 function insert_user( $name, $pw ){
 	$encode = sha1( $pw );
 	$query="insert into pwdemo values ('$name', '$encode')";
-	$dbc = connect_to_db("jed");
-	$result = perform_query($dbc, $query);
-	if (!$result)
+	$dbc = connect_to_db( "jed" );
+	$result = perform_query( $dbc, $query );
+	if ( !$result )
 		echo "<br>Insert Failed - $query";
 	else
 		echo "<br>Insert Success- $query";
@@ -73,14 +75,14 @@ function insert_user( $name, $pw ){
 function validate_user( $name, $pw ){
 	$encode = sha1( $pw );
 	$query = "select * from pwdemo where name='$name' and pass='$encode'";
-	$dbc = connect_to_db("jed");
-	$result = perform_query($dbc, $query);
-	$row =mysqli_fetch_array($result, MYSQLI_ASSOC);
+	$dbc = connect_to_db( "jed" );
+	$result = perform_query( $dbc, $query );
+	$row =mysqli_fetch_array( $result, MYSQLI_ASSOC );
 	echo "<pre>";
 	print_r($row);
 	echo "</pre>";
 	
-	if (mysqli_num_rows($result) == 0)
+	if ( mysqli_num_rows( $result ) == 0)
 		echo "<br>Validate Failure - $query";
 	else
 		echo "<br>Validate Success - $query";
--- W6_Password/sha1demo.php
+++ W6_Password/sha1demo.php
@@ -2,32 +2,31 @@
 <html>
 <head>
 	<title>sha1 Demo</title>
-		<link rel="stylesheet" href="css/base.css" />
+	<link rel="stylesheet" href="css/base.css" />
 </head>
 <body>
-<pre><?php print_r($_GET); ?></pre>
+	<pre><?php print_r($_GET); ?></pre>
 	<h1>sha1 demo</h1>
-
-<?
-	if ( isset( $_GET['submit'] ) ) {
-		handle_form( $_GET['input1'], $_GET['input2'] );
-	}
-	display_form();
-?>
+	<?php
+		if ( isset( $_GET['submit'] ) ) {
+			handle_form( $_GET['input1'], $_GET['input2'] );
+		}
+		display_form();
+	?>
 </body>
 </html>
-<?
+<?php
 function display_form(){
 ?>
 <form method="get">
-Text to Encode: 
-<input name="input1" size="20" value="
-<?php echo isset($_GET['input1']) ? $_GET['input1'] : ""; ?>" type="text" />
-<input name="input2" size="20" value="
-<?php echo isset($_GET['input2']) ? $_GET['input2'] : ""; ?>" type="text" />
-<input type="submit" name="submit" value="encode" />
+	Text to Encode: 
+	<input name="input1" size="20" value="
+	<?php echo isset($_GET['input1']) ? $_GET['input1'] : ""; ?>" type="text" />
+	<input name="input2" size="20" value="
+	<?php echo isset($_GET['input2']) ? $_GET['input2'] : ""; ?>" type="text" />
+	<input type="submit" name="submit" value="encode" />
 </form>
-<?
+<?php
 }
 
 function handle_form( $input1, $input2 ){ 
