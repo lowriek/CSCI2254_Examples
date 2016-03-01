6d801441201ca2906850b0772934b87e8220a481
lowriek <lowriek@bc.edu>
2016-03-01 09:31:00 -0500

    Added multiple feeds and select menu

--- W7_RSS/xml_from_the_web.php
+++ W7_RSS/xml_from_the_web.php
@@ -1,26 +1,38 @@
 <html>
-  <head>
+<head>
     <title>XML demo 3: RSS News Viewer</title>
-    <link rel="stylesheet" type="text/css" href="css/baseA.css" />	  
-  </head>
-  <style>		
-  
-  .news {
-			overflow: auto;
-			height: 300px;
-			border: 3px groove blue;	
-		}
-		
+     <style>		
+	  .news {
+				overflow: auto;
+				height: 300px;
+				border: 3px groove blue;	
+	 }
   </style>
+</head>
+<body>
+  <?php
+  $feeds = array('http://rss.nytimes.com/services/xml/rss/nyt/Space.xml',
+				  'http://www.usa.gov/rss/updates.xml',
+				  'http://www.nasa.gov/rss/dyn/image_of_the_day.rss',
+				  'http://cscilab.bc.edu/~csinsider/?feed=rss2'
+				);
+  print_r($_GET);
+  ?>
+  <h1>RSS Feeds</h1>
+  <?php
+  create_form($feeds, "feed");
   
-  <body>
-    <?php 
-    $rss= new SimpleXMLElement(file_get_contents('http://rss.nytimes.com/services/xml/rss/nyt/Space.xml'));
-    
-      // Load and parse the XML document
-      //$rss =  simplexml_load_file('http://www.usa.gov/rss/updates.xml');
-     // $rss =  simplexml_load_file('http://www.nasa.gov/rss/dyn/image_of_the_day.rss');
- 	  //$rss =  simplexml_load_file(c);
+  if ( isset( $_GET['getfeed'] ) ) {
+  	handle_form( $_GET['feed'] );	
+  }
+  
+  ?>
+  </body>
+</html>
+<?php
+function handle_form( $myfeed ) {
+
+	  $rss = simplexml_load_file( $myfeed );
  	  
       $title =  $rss->channel->title;
       echo "<h1>$title</h1>";
@@ -35,14 +47,28 @@
       if (!$items)
         $items = $rss->item; # works other versions
 
-      foreach ($items as $item) {
+      foreach ( $items as $item ) {
       	echo "<div class='news'>
       			<h2>$item->title<h2>\n";
         echo '<a href="' . $item->link . '">' . $item->title . '</a><br>';
         echo $item->description . "<br><br>\n";
-        echo "<hr></div>";
+        echo "</div>";
       }
-    ?>
-	
-  </body>
-</html>
+}
+
+function create_form( $farray, $menuname ){
+?>
+<form method="get">
+	<?php create_menu( $farray, $menuname ); ?>
+	<input type="submit" name="getfeed" value="Get Feed!!">
+</form>
+<?php
+}
+function create_menu($farray, $menuname){
+					
+	echo "<select name='$menuname'>";
+	foreach ( $farray as $f ) {
+		echo "<option value='$f'>$f</option>";
+	}
+	echo '</select>';
+}
