b19846d647cd59620814db7335f9ce2ae2c36c2f
K Lowrie <lowriekate@gmail.com>
2016-03-01 09:29:31 -0500

    Added RSS with one feed, first rss example

---
+++ W7_RSS/first_rss.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>RSS Reader</title>
+	<link rel="stylesheet" type="text/css" href="css/baseA.css" />	  
+</head>
+<body>
+<?php // Load and parse the XML document
+	$rss =  simplexml_load_file('http://syndication.boston.com/news?mode=rss_10');
+	$title =  $rss->channel->title;
+	echo "<h1>$title</h1>";
+
+	# I would like to do this:
+	#     foreach ($rss->channel->item as $item) {
+	# or this:
+	#     foreach ($rss->item as $item) {
+	# but which one depends on the rss version in use.
+
+	$items = $rss->channel->item; # try, works some versions
+	if (!$items)
+		$items = $rss->item; # works other versions
+
+	foreach ($items as $item) {
+        echo '<a href="' . $item->link . '">' . $item->title . '</a><br>';
+        echo $item->description . "\n";
+	}
+?>
+</body>
+</html>
