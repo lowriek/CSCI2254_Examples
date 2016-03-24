907b4c644279c0b85c391c887efa04f8e3a39daa
K Lowrie <lowriekate@gmail.com>
2016-03-24 10:06:26 -0400

    example
    works for two records returned

--- W9_AJAX/ajaxmysqltable/mysqljsonMR.php
+++ W9_AJAX/ajaxmysqltable/mysqljsonMR.php
@@ -2,13 +2,16 @@
 	
 	$dbc = @mysqli_connect("localhost", "root", "root", "wfb2007")
 	       or die("Could not open wfb2007 db, " . mysqli_connect_error());
-	$query = "select CountryName, Languages, Background from countries 
-						where CountryName='Germany' or CountryName='France'";
+	$query = "select CountryName, Languages, Background from countries where CountryName='Germany' or CountryName='France'";				
+
 	$result = mysqli_query($dbc, $query);
 	if ( mysqli_num_rows( $result ) == 0 ) {
 		die("bad query $query");
 	}
-	$row = mysqli_fetch_array($result);
-	$country_data = array('countryInfo' => $row);
-	echo json_encode($country_data);
+	$country_data = array();	// put the rows as objects in an array
+	while ( $row = mysqli_fetch_row( $result ) ) {
+		$country_data[] = $row;
+	}
+	echo '{"countries":'.json_encode($country_data).'}';
+
 	mysqli_close($dbc);
