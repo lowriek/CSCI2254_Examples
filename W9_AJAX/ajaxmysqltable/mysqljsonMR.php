20955881d833d3990cd3341c82da8c2866a99e50
K Lowrie <lowriekate@gmail.com>
2016-03-27 07:30:31 -0400

    added order by, clarified var name

--- W9_AJAX/ajaxmysqltable/mysqljsonMR.php
+++ W9_AJAX/ajaxmysqltable/mysqljsonMR.php
@@ -2,15 +2,15 @@
 	
 	$dbc = @mysqli_connect("localhost", "root", "root", "wfb2007")
 	       or die("Could not open wfb2007 db, " . mysqli_connect_error());
-	$query = "select CountryName, Continent from countries";				
+	$query = "select CountryName, Continent from countries order by Continent, CountryName";				
 
 	$result = mysqli_query($dbc, $query);
 	if ( mysqli_num_rows( $result ) == 0 ) {
 		die("bad query $query");
 	}
 	$country_data = array();	// put the rows as objects in an array
-	while ( $row = mysqli_fetch_object( $result ) ) {
-		$country_data[] = $row;
+	while ( $obj = mysqli_fetch_object( $result ) ) {
+		$country_data[] = $obj;
 	}
 	echo json_encode($country_data);
 
